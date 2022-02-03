0f47b5b1ef193753c02965cdcee1cbb656ca2cb7
Ludoug-f <lucianoguns123@gmail.com>
2022-02-03 16:55:56 -0300

    exercicios

---
+++ Bloco4_Introdução_JS_Lógica/array_e_loop_for/exercicios_forof.js
@@ -0,0 +1,79 @@
+//let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+//for (const num of numbers) {
+//    console.log(num);
+//}
+
+
+//let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+//let result = 0;
+//for (let index = 0; index < numbers.length; index+=1) {
+//    result+=numbers[index];
+//    }
+//    console.log(result);
+
+
+//let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+//let soma = 0;
+//
+//for (let index = 0; index < numbers.length; index+=1) {
+//    soma+=numbers[index];
+//    }
+//    let result = soma/numbers.length;
+//    console.log(result);
+
+
+
+//let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+//let soma = 0;
+//
+//for (let index = 0; index < numbers.length; index+=1) {
+//    soma+=numbers[index];
+//    }
+//    let result = soma/numbers.length;
+//    if (result >20) {
+//        console.log("valor maior que 20");
+//    } else {
+//        console.log("valor menos ou igual a 20");
+//    }
+
+
+
+//let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+//let highNumber = numbers[0]
+//for (let index = 1; index < numbers.length; index+=1) {
+//    if (numbers[index] > highNumber) {
+//        highNumber = numbers[index]
+//    }
+//
+//}
+//console.log(highNumber);
+
+
+
+//let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+//let result = 0
+//for (let index = 0; index < numbers.length; index+=1) {
+//    if (numbers[index] % 2 !== 0) {
+//        result += 1
+//    }
+//    ;
+//}
+//if (result!=0) {
+//    console.log(result);    
+//} else {
+//    console.log('nenhum valor ímpar encontrado');
+//}
+
+
+
+//let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+//let result = numbers[0];
+//
+//for (let index = 1; index < numbers.length; index += 1) {
+//    if (numbers[index] < result ) {
+//        result = numbers[index]
+//    }
+//
+//}
+//
+//console.log(result);
