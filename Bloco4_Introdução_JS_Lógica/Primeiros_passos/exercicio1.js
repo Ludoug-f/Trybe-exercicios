663b5ac4c283a9bf2bee5537e07c6b6249bf07e3
Ludoug-f <lucianoguns123@gmail.com>
2022-02-03 14:59:48 -0300

    exercicio1

---
+++ Bloco4_Introdução_JS_Lógica/Primeiros_passos/exercicio1.js
@@ -0,0 +1,76 @@
+//const a = 5
+//const b = 10
+//console.log(a+b);
+
+
+
+//const a = 5
+//const b = 10
+//console.log(a-b);
+
+
+
+//const a = 5
+//const b = 10
+//console.log(a*b);
+
+
+
+//const a = 5
+//const b = 10
+//console.log(a/b);
+
+
+
+//const a = 5
+//const b = 10
+//console.log(a%b);
+
+
+
+//const a = 5
+//const b = 10
+//if (a>b) {
+//    console.log(a);
+//} else {
+//    console.log(null);
+//}
+
+
+
+//const a= 5
+//const b= 10
+//const c= 15
+//if (a>b && a>c) {
+//    console.log(a);
+//} else if (b>a && b>c){ 
+//    console.log(b);
+//} else if (c>a && c>b){ 
+//    console.log(c);
+//}
+
+
+
+//const number = -5
+//if (number > 0) {
+//    console.log("positive");
+//} else if (number<0){ 
+//    console.log("negative");
+//} else if (number==0){ 
+//    console.log("zero");
+//}
+
+
+
+//const a = 80;
+//const b = 60;
+//const c = 50;
+//var ang = a+b+c;
+//var result;
+//if (ang == 180) {
+//    var result = true
+//    console.log(result);
+//} else {
+//    var result = false
+//    console.log(result);
+//}
