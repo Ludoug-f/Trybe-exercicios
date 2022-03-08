946748966910785717a376d602975b92152c5ca5
Luciano_Freitas <64561258+Ludoug-f@users.noreply.github.com>
2022-03-08 14:00:45 -0300

    exerc1

--- Bloco7_ES6_testes_unitarios/1_let_const_arrowfunc_templateli/part1/exerc1.js
+++ Bloco7_ES6_testes_unitarios/1_let_const_arrowfunc_templateli/part1/exerc1.js
@@ -1,7 +1,7 @@
 const testingScope = escopo => {
   if (escopo === true) {
     let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
-    ifScope = '${ifScope} ótimo, fui utilizada no escopo !';
+    ifScope = `${ifScope} ótimo, fui utilizada no escopo !`;
     console.log(ifScope);
   } else {
     const elseScope = 'Não devo ser utilizada fora meu escopo (else)';
@@ -10,3 +10,9 @@ const testingScope = escopo => {
 }
 
 testingScope(true);
+
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+// Seu código aqui.
+
+console.log(oddsAndEvens); // será necessário alterar essa linha 😉
