d81ace666503cce15b0da7666c251a18a1b5d3c5
Luciano_Freitas <64561258+Ludoug-f@users.noreply.github.com>
2022-04-05 15:34:00 -0300

    exercício fixação

--- 2_Desenvolvimento_Front-end/Bloco10_Introdução_a_React/01_Introdução_React/testando-meu-pc/src/App.js
+++ 2_Desenvolvimento_Front-end/Bloco10_Introdução_a_React/01_Introdução_React/testando-meu-pc/src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import HelloWorld from './HelloWorld';
 import './App.css';
+import Image from './image';
 
 function App() {
-  return <HelloWorld/>;
+  return (
+  <main> <HelloWorld/>
+  <Image source = 'https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg' alternativeText = 'Cute cat staring' />
+  </main>
+  )
 }
 
 export default App;
