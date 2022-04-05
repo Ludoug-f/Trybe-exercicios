df8cf808f5163b91a7184c4f74230b5984c834e3
Luciano_Freitas <64561258+Ludoug-f@users.noreply.github.com>
2022-04-05 14:43:22 -0300

    HelloWorld no React

---
+++ 2_Desenvolvimento_Front-end/Bloco10_Introdução_a_React/01_Introdução_React/testando-meu-pc/src/HelloWorld.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class HelloWorld extends Component {
+  render() {
+    return <div> <h1> Luciano Freitas </h1> <p> nerdola, nerdola, nerdola, nerdola, nerdola, nerdola, nerdola, nerdola </p> </div>;
+    }
+}
+
+export default HelloWorld;
